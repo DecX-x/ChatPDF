a22bcee398887adc970764061b5bdce4a83b2beb
Muhammad Ellbendl Satria <69744381+DecX-x@users.noreply.github.com>
2023-06-25 20:30:42 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,54 @@
+from dotenv import load_dotenv
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
+import os
+
+def main():
+    os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
+    st.set_page_config(page_title="PdfGPT")
+    st.header("Ask your PDF 💀")
+    
+    # upload
+    pdf = st.file_uploader("Upload your File", type="pdf")
+    
+    # extract
+    if pdf is not None:
+      pdf_reader = PdfReader(pdf)
+      text = ""
+      for page in pdf_reader.pages:
+        text += page.extract_text()
+        
+      #chunks
+      text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+      )
+      chunks = text_splitter.split_text(text)
+      
+      #embeddings
+      embeddings = OpenAIEmbeddings()
+      knowledge_base = FAISS.from_texts(chunks, embeddings)
+      
+      user_question = st.text_input("Ask your PDF:")
+      if user_question:
+        docs = knowledge_base.similarity_search(user_question)
+        
+        llm = OpenAI()
+        chain = load_qa_chain(llm, chain_type="stuff")
+        with get_openai_callback() as cb:
+          response = chain.run(input_documents=docs, question=user_question)
+          print(cb)
+           
+        st.write(response)
+    
+
+if __name__ == '__main__':
+    main()
